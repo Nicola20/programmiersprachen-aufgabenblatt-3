d1e8b46d009f9316bbd12237987812487030105f
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-05-23 15:32:39 +0200

    finished 3.6

--- source/aufgabe_6.cpp
+++ source/aufgabe_6.cpp
@@ -1,2 +1,31 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
 #include "Circle.hpp"
-#include 
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+
+TEST_CASE ("test_if_sorted","[sorting]")
+ {
+   std::vector<Circle> sorted_circles;
+
+ Circle c1 {4.0f,Vec2{3.7f, 6.3f}, Color{1.0f, 1.0f,1.0f}};
+ Circle c2 {4.7f,Vec2{3.7f, 6.3f}, Color{1.0f, 1.0f,1.0f}};
+ Circle c3 {6.9f,Vec2{3.7f, 6.3f}, Color{1.0f, 1.0f,1.0f}};
+
+ sorted_circles.push_back(c1);
+ sorted_circles.push_back(c2);
+ sorted_circles.push_back(c3);
+
+ std::sort(sorted_circles.begin(), sorted_circles.end());
+ 
+   REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
+ } 
+
+int main(int argc, char *argv[])
+{
+  return Catch::Session().run(argc, argv);
+	//return 0;
+}
